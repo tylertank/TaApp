1ef9b55f996f57b22a131a8633b66f47d6ca0b8c
ColeHanlon <cole.hanlon28@gmail.com>
2022-09-28 12:19:42 -0600

    Updated email sender

--- TAApplication/Areas/Identity/Services/EmailSender.cs
+++ TAApplication/Areas/Identity/Services/EmailSender.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿/**
+/**
  * Author:    Cole Hanlon
  * Partner:   Tyler Harkness
  * Date:      9/27/2022
@@ -14,22 +13,6 @@
  *   Defines an email sending object, and executes sending of emails. Provided by Microsoft.
  */
 
-=======
-/*< !--
-    Author:    Cole Hanlon
-Partner:   Tyler Harkness
-Date: 9 / 27 / 2022
-Course:    CS 4540, University of Utah, School of Computing
-Copyright: CS 4540 and Cole Hanlon, Tyler Harkness - This work may not be copied for use in Academic Coursework.
-
-    I, Cole Hanlon & Tyler Harkness, certify that I wrote this code from scratch and did not copy it in part or whole from
-  another source.Any references used in the completion of the assignment are cited in my README file.
-
-  File Contents
-
-      This is the email sender class, provides the framework so users can confirm their emails and reset passwords.
--->*/﻿
->>>>>>> 677f77c8994c07ae3133ae68d3ebc72edbee68f3
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
 using SendGrid;
