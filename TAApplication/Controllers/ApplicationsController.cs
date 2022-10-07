d1556e2c627eba058c225407c8e4e3ebb62f87a3
TYLER HARKNESS <tylerharkness@hotmail.com>
2022-10-07 14:21:39 -0600

    fixed application create

--- TAApplication/Controllers/ApplicationsController.cs
+++ TAApplication/Controllers/ApplicationsController.cs
@@ -80,6 +80,11 @@ namespace TAApplication.Controllers
         }
 
 
+
+       
+        
+
+
         // POST: Applications/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
