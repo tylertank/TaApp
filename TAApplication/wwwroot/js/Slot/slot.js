96fd9b998ce341d477208cd2fa060039a32cfda8
ColeHanlon <cole.hanlon28@gmail.com>
2022-11-18 17:35:25 -0700

    Finished basic slot drawing

--- TAApplication/wwwroot/js/Slot/slot.js
+++ TAApplication/wwwroot/js/Slot/slot.js
@@ -2,16 +2,30 @@
 
     constructor(x, y, width, height, color) {
         super();
+        this.saved_x = x;
+        this.saved_y = y;
+        this.saved_width = width;
+        this.saved_height = height;
+        this.saved_color = color;
 
         this.draw_self(x, y, width, height, color);
-        //this.interactive = true;
-        //this.on('mousedown', this.mouse_handler);
-        //this.buttonMode = true;
+        this.interactive = true;
+        this.on('mousedown', this.mouse_handler);
+        this.buttonMode = true;
     }
 
     mouse_handler() {
         console.log("here");
-        //this.color = 0x01BB2F;
+        if (this.saved_color == 0x01BB2F) {
+            this.saved_color = 0xCB0000;
+        }
+        else {
+            this.saved_color = 0x01BB2F
+        }
+        this.lineStyle(1, this.saved_color);
+        this.beginFill(this.saved_color);
+        this.drawRect(this.saved_x, this.saved_y, this.saved_height, this.saved_width);
+        this.endFill();
     }
 
     draw_self(x, y, width, height, color) {
