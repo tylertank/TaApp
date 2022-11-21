3ce7a5319dcc39fc52124e40f896df8f01b061c0
TYLER HARKNESS <tylerharkness@hotmail.com>
2022-11-21 11:20:11 -0700

    gets each user's avaliability.

--- TAApplication/wwwroot/js/Slot/update.js
+++ TAApplication/wwwroot/js/Slot/update.js
@@ -1,18 +1,17 @@
-﻿$(document).ready(function getAvaliability() {
-
-
-    $.get("../GetAvailabilities/u0000001", function (myList) {
+﻿
+function getAvaliability1(data) {
+    $.get("../GetAvailabilities/"+data, function (myList) {
         const slots = app.stage.children;
         const userAvaliability = myList.myList;
         var numOfUser = userAvaliability.length;
-    
+
         for (var i = 0; i < slots.length; i++) {
             for (var j = 0; j < userAvaliability.length; j++) {
                 if (slots[i].ID == userAvaliability[j].time) {
-                slots[i].changeColor();
-            }
+                    
+                    slots[i].changeColor();
+                }
             }
         }
     });
-});
-
+}
